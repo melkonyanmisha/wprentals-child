a4a9454adcd27c325f58a1c4c8e931cb40fa1cfb
melkonyanmisha <melkonyan.misha@gmail.com>
2023-10-13 13:26:27 +0400

    Handle dropdowns for fees in "My Listings" page

--- dashboard/functions.php
+++ dashboard/functions.php
@@ -10,3 +10,44 @@ function wpestate_check_user_level(): bool
 {
     return current_user_is_admin();
 }
+
+/**
+ * Handle dropdowns for fees
+ *
+ * @param string $name
+ * @param float $selected
+ * @param int $rental_type
+ * @param int $booking_type
+ *
+ * @return string
+ */
+function wpestate_dropdown_fee_select(string $name, float $selected, int $rental_type, int $booking_type): string
+{
+    $options_array = [
+        0 => esc_html__('Single Fee', 'wprentals'),
+        1 => ucfirst(wpestate_show_labels('per_night', $rental_type, $booking_type)),
+        2 => esc_html__('Per Guest', 'wprentals'),
+        3 => ucfirst(wpestate_show_labels('per_night', $rental_type, $booking_type)) . ' ' . esc_html__(
+                'Per Guest',
+                'wprentals'
+            )
+    ];
+
+    // Start output buffering
+    ob_start();
+    ?>
+
+    <select class="select_submit_price" name="<?= esc_attr($name); ?>" id="<?= esc_attr($name); ?>">
+        <?php
+        foreach ($options_array as $key => $option) { ?>
+            <option value="<?= esc_attr($key); ?>" <?= $key == $selected ? 'selected' : ''; ?> >
+                <?= esc_html($option); ?>
+            </option>
+            <?php
+        } ?>
+    </select>
+
+    <?php
+    // End output buffering
+    return trim(ob_get_clean());
+}
